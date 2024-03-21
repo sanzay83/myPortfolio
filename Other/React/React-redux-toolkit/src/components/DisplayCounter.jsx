bae4796c28decfb5795a3904189dc6fffb6d60dd
Sanjay Duwal <sanzay8342258@gmail.com>
2024-03-20 19:21:10 -0500

    redux-complete

--- Other/React/React-redux-toolkit/src/components/DisplayCounter.jsx
+++ Other/React/React-redux-toolkit/src/components/DisplayCounter.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function DisplayCounter() {
-  const counter = useSelector((store) => store.counter);
+  const counterObj = useSelector((store) => store.counter);
+  const counter = counterObj.counterVal;
   return (
     <div>
       <p className="lead mb-4">Counter Current Value: {counter}</p>
--- Other/React/React-redux-toolkit/src/store/index.js
+++ Other/React/React-redux-toolkit/src/store/index.js
@@ -1,31 +1,12 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import counterSlice from "./counter";
+import privacySlice from "./privacy";
 
-const INITIAL_VALUE = {
-  counter: 0,
-  privacy: false,
-};
-
-const counterReducer = (store = INITIAL_VALUE, action) => {
-  if (action.type === "INCREMENT") {
-    return { ...store, counter: store.counter + 1 };
-  } else if (action.type === "DECREMENT") {
-    return { ...store, counter: store.counter - 1 };
-  } else if (action.type === "ADD") {
-    return {
-      ...store,
-      counter: store.counter + Number(action.payload.num),
-    };
-  } else if (action.type === "SUBTRACT") {
-    return {
-      ...store,
-      counter: store.counter - Number(action.payload.num),
-    };
-  } else if (action.type === "PRIVACYTOGGLE") {
-    return { ...store, privacy: !store.privacy };
-  }
-  return store;
-};
-
-const counterStore = createStore(counterReducer);
+const counterStore = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    privacy: privacySlice.reducer,
+  },
+});
 
 export default counterStore;
