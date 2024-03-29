049f746bf318a4d5e48da1aa7126e199710fbf7b
Sanjay Duwal <sanzay8342258@gmail.com>
2024-03-28 23:00:58 -0500

    removing more useless files

--- Other/React/Calculator/src/Component/ButtonContainer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './ButtonContainer.module.css'
-const ButtonContainer = ({onButtonClick}) => {
-  const buttonNames = ['AC','M+','M-','C','1','2','3','+','4','5','6','-','7','8','9','*','0','.','=','/']
-  return <div className={styles.buttonsContainer}>
-    {buttonNames.map(buttonName => (<button key={buttonName} onClick={() => onButtonClick(buttonName)} className={styles.button}>{buttonName}</button>))}
-</div>
-}
-
-export default ButtonContainer;
--- Other/React/Social Media/src/components/WelcomeMessage.jsx
+++
@@ -1,9 +0,0 @@
-const WelcomeMessage = () => {
-  return (
-    <h1 className="welcome-message">
-      There are no posts <br />
-    </h1>
-  );
-};
-
-export default WelcomeMessage;
